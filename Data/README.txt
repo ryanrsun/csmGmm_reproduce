5c4097af114a644a8c3f20133d41d8806558a382
Ryan Sun <ryanrsun@gmail.com>
2024-10-02 21:47:44 -0500

    now using wget

--- Data/README.txt
+++ Data/README.txt
@@ -1,6 +1,8 @@
 The other_data_locations.txt file provides information about whether other data files are hosted.
 
-These data files should be downloaded into your output directory. The R scripts will look for them there. 
+These data files should be downloaded into this data directory. The R scripts will look for them there. 
+
+We have also provided the download_data.sh file. If you run this, (e.g. cd into the data/ folder from the terminal, then type "./download_data.sh") then it will automatically download all the data for you.
 
 
 
---
+++ Data/download_data.sh
@@ -0,0 +1,11 @@
+wget "https://www.dropbox.com/scl/fi/c5ftmij2hsuw45mxlfsk8/bmi_with_overall.txt?rlkey=ztdjk8jja1076p1eo1dzdq6zh&st=y2rs6ed8&dl=1" -O bmi_with_overall.txt
+
+wget "https://www.dropbox.com/scl/fi/bl3j9afhwnaxoydpvio0h/cad_for_replication.txt?rlkey=4c4tt8oo3csnu785qspadzduu&st=yr7kafvn&dl=1" -O cad_for_replication.txt
+
+wget "https://www.dropbox.com/scl/fi/pszo9ojmsod5wv4rh3sx4/replication_with_lcoverall.txt?rlkey=exwjwbc9e4olk44xgh19xnjnz&st=2oo3mauy&dl=1" -O replication_with_lcoverall.txt
+
+wget "https://www.dropbox.com/scl/fi/ftrbybngfat6y0fe3q1lc/scc_lung_addchr1.csv?rlkey=2gct1647k21k9uw9uqxwcrzxh&st=2jo3cmu8&dl=1" -O scc_lung_addchr1.csv
+
+wget "https://www.dropbox.com/scl/fi/l6dk45obfgh8uxxl3f67q/synthDat.txt?rlkey=3mzrf7a8oogcz30itp3voih6r&st=k0w364ka&dl=1" -O synthDat.txt
+
+wget "https://www.dropbox.com/scl/fi/kbnmmsr82ybah3bg0l4kk/eqtl_results.zip?rlkey=rurppk5d80dxihge2emrzr17i&st=pp8owbdp&dl=1" -O eqtl_results.zip      
