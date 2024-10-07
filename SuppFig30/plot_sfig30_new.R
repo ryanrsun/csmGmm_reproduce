ee71423f85ef036cd9ae6710efb7b4936ddc5ebe
Ryan Sun <ryanrsun@gmail.com>
2024-10-06 23:13:01 -0500

    add here package

--- SuppFig30/plot_sfig30_new.R
+++ SuppFig30/plot_sfig30_new.R
@@ -1,21 +1,25 @@
 # Collect results and plot Supp Figure 30
+
+# Using the here package to manage file paths. If an error is thrown, please
+# set the working directory to the folder that holds this Rscript, e.g.
+# setwd("/path/to/csmGmm_reproduce/SuppFig30/plot_sfig30_new.R") or set the path after the -cwd flag
+# in the .lsf file, and then run again.
+here::i_am("SuppFig30/plot_sfig30_new.R")
+
 library(ggplot2)
 library(cowplot)
 library(ggformula)
 library(dplyr)
 library(data.table)
-setwd('/rsrch3/home/biostatistics/rsun3/github/ancillaryFunctions')
-file.sources = list.files(pattern="*.R")
-sapply(file.sources,source,.GlobalEnv)
 
-#-----------------------------------------#
-# change to where the output files are stored
-outputDir <- "/rsrch3/home/biostatistics/rsun3/empBayes/reproduce/SuppFig29/output"
-names29f1 <- paste0("SFig29E_aID", 1:1000, "_fithigh.txt")
-names29f3 <- paste0("SFig29E_aID", 1:1000, "_fitlow.txt")
-#-----------------------------------------#
+# source the .R scripts from the SupportingCode/ folder 
+codePath <- c(here::here("SupportingCode"))
+toBeSourced <- list.files(codePath, "\\.R$")
+purrr::map(paste0(codePath, "/", toBeSourced), source)
 
-#------------------------------------------------#
+# set output directory 
+outputDir <- here::here("SuppFig29", "output/")
+
 # plotting starts
 
 # define colors
@@ -29,19 +33,19 @@ mycols[5] <- "blue"
 
 # read data
 setwd(outputDir)
-med2d_mbl5_use1_high <- fread("med_mbl5_onemix_high.txt", data.table=F) %>%
+med2d_mbl5_use1_high <- fread(paste0(outputDir, "med_mbl5_onemix_high.txt"), data.table=F) %>%
   filter(Method == "New") %>%
   mutate(Method = "csmGmm-1-high") %>%
   select(minEff1, Method, Power, FDP)
-med2d_mbl5_use1_low <- fread("med_mbl5_onemix_low.txt", data.table=F) %>%
+med2d_mbl5_use1_low <- fread(paste0(outputDir, "med_mbl5_onemix_low.txt"), data.table=F) %>%
   filter(Method == "New")  %>%
   mutate(Method = "csmGmm-1-low") %>%
   select(minEff1, Method, Power, FDP)
-med2d_mbl5_use1_orig <- fread("med2d_changeeff_mbltrue5_use1_2pct_init.txt", data.table=F) %>%
+med2d_mbl5_use1_orig <- fread(here::here("SuppFig26/output/med2d_changeeff_mbltrue5_use1_2pct_init.txt"), data.table=F) %>%
   filter(Method == "New")  %>%
   mutate(Method = "csmGmm-1-med") %>%
   select(minEff1, Method, Power, FDP)
-med2d_mbl5_standard <- fread("med2d_changeeff_mbltrue5_use1_2pct_double_init.txt", data.table=F) %>%
+med2d_mbl5_standard <- read(here::here("SuppFig26/output/med2d_changeeff_mbltrue5_use1_2pct_double_init.txt"), data.table=F) %>%
   filter(Method == "New")  %>%
   mutate(Method = "csmGmm-standard") %>%
   select(minEff1, Method, Power, FDP)
@@ -66,8 +70,7 @@ med2d_mbl5_OM_power_plot <- ggplot(data=med2d_mbl5_OM, aes(x=minEff1, y=Power, g
 med2d_mbl5_OM_power_plot
 
 # save
-setwd(outputDir)
-ggsave("med2d_mbl5_onemix_power.pdf", width=12, height=6)
+#ggsave("med2d_mbl5_onemix_power.pdf", width=12, height=6)
 
 
 
