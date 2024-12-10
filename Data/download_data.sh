21590183d9169be68628167a043aeae0ff2dbf6c
Ryan Sun <ryanrsun@gmail.com>
2024-12-09 22:09:58 -0600

    moved data to odin

--- Data/download_data.sh
+++ Data/download_data.sh
@@ -1,13 +1,13 @@
-wget "https://www.dropbox.com/scl/fi/c5ftmij2hsuw45mxlfsk8/bmi_with_overall.txt?rlkey=ztdjk8jja1076p1eo1dzdq6zh&st=y2rs6ed8&dl=1" -O bmi_with_overall.txt
+wget "https://odin.mdacc.tmc.edu/~rsun3/bmi_with_overall.txt" -O bmi_with_overall.txt
 
-wget "https://www.dropbox.com/scl/fi/bl3j9afhwnaxoydpvio0h/cad_for_replication.txt?rlkey=4c4tt8oo3csnu785qspadzduu&st=yr7kafvn&dl=1" -O cad_for_replication.txt
+wget "https://odin.mdacc.tmc.edu/~rsun3/cad_for_replication.txt" -O cad_for_replication.txt
 
-wget "https://www.dropbox.com/scl/fi/pszo9ojmsod5wv4rh3sx4/replication_with_lcoverall.txt?rlkey=exwjwbc9e4olk44xgh19xnjnz&st=2oo3mauy&dl=1" -O replication_with_lcoverall.txt
+wget "https://odin.mdacc.tmc.edu/~rsun3/replication_with_lcoverall.txt" -O replication_with_lcoverall.txt
 
-wget "https://www.dropbox.com/scl/fi/ftrbybngfat6y0fe3q1lc/scc_lung_addchr1.csv?rlkey=2gct1647k21k9uw9uqxwcrzxh&st=2jo3cmu8&dl=1" -O scc_lung_addchr1.csv
+wget "https://odin.mdacc.tmc.edu/~rsun3/scc_lung_addchr1.csv" -O scc_lung_addchr1.csv
 
-wget "https://www.dropbox.com/scl/fi/l6dk45obfgh8uxxl3f67q/synthDat.txt?rlkey=3mzrf7a8oogcz30itp3voih6r&st=k0w364ka&dl=1" -O synthDat.txt
+wget "https://odin.mdacc.tmc.edu/~rsun3/synthDat.txt" -O synthDat.txt
 
-wget "https://www.dropbox.com/scl/fi/kbnmmsr82ybah3bg0l4kk/eqtl_results.zip?rlkey=rurppk5d80dxihge2emrzr17i&st=pp8owbdp&dl=1" -O eqtl_results.zip      
+wget "https://odin.mdacc.tmc.edu/~rsun3/eqtl_results.zip" -O eqtl_results.zip      
 
-Wget "https://www.dropbox.com/scl/fi/o6ts52z7sl37umxsq99k7/cleanG_set1_dataset1.txt?rlkey=hu6j5oav6deq0uh0dfkcog904&st=uf9v96sz&dl=1" -O cleanG_set1_dataset1.txt 
+Wget "https://odin.mdacc.tmc.edu/~rsun3/cleanG_set1_dataset1.txt" -O cleanG_set1_dataset1.txt 
